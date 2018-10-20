fdc05c33ad51525ebefeaff9e7bf04bf8452840d
SoyoungLee <soyoung.dev@gmail.com>
2018-10-20 20:09:08 +0900

    2018-10-20 Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const sampleText = " printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'"
+const text = sampleText.split('');
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>KHUVELOPER PROJECT</h1>
+        <div className="text-area">
+          {text.map((item, i) => (
+            <span style={{
+              color: `rgb(${i/2},${i/2},${i/2})`
+            }}>{item}</span>
+          ))}
+        </div>
       </div>
     );
   }
