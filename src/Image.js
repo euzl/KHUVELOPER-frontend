25096b3bc04f4b1542ecdaaeb36bb50ccef02f64
euzl <euzl@khu.ac.kr>
2018-11-11 16:35:21 +0900

    거의 됐지롱~~!

--- src/Image.js
+++ src/Image.js
@@ -6,35 +6,43 @@ import * as reducer from './store';
 
 import { color, text } from './color';
 
+
 class Image extends Component {
   constructor() {
     super();
     this.state = {
-      sentence: (Array(30).join(text)).substr(0, 4700),
+      sentence: (Array(30).join(text)).substr(0, 7000), // substr(arg1,arg2) -> arg2에 기본으로 출력될 문자 수 입력
     }
   }
 
   componentWillReceiveProps(nextProps) {
     const { typingStatus, word } = nextProps;
     const { typingNone } = this.props;
-    console.log(typingStatus);
+    let { sentence } = this.state;
+
+    // typing이 끝났을 때
     if (typingStatus === 'done') {
+      // 최대 길이를 초과했을 때, 앞의 한 글자 삭제
+      if(sentence.length >= 7000) sentence = sentence.substr(1,7001) // 1 ~ arg2 까지만 출력. (arg2=7001)
+      // state의 sentence에 새 단어 추가
       this.setState({
-        sentence: this.state.sentence.concat(word),
+       sentence: sentence + word,
       })
+
+      // store의 typing 상태 변경
       return typingNone();
     }
   }
 
-  render() {
-    const colorList = color.split(':');
+  render(){
+    const colorList = color.split(':'); // ':'로 rgb 나눔
     const textList = this.state.sentence.split('');
     return (
       <Fragment>
       <div style = {{textAlign: 'left',}}>
         {textList.map((item, i) => (
           <div className="word" style={{
-            color: colorList[i],
+            color: colorList[i], // word글자에 색 넣기
              //backgroundColor: colorList[i],
           }} key={i}>{item}</div>
         ))}
@@ -49,6 +57,7 @@ export default connect(
   state => ({
     typingStatus: state.typingStatus,
     word: state.word,
+    //splitWord: state.splitWord,
   }),
   dispatch => ({
     typingNone: bindActionCreators(reducer.typingNone, dispatch)
